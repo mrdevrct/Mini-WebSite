686aeba74a424d7a5ba05d8366c0eb0dae6de3e6
Devrct-Front <raonbowcoffeepubg@gmail.com>
2024-01-30 15:46:07 +0330

    first commit

---
+++ assest/js/router.js
@@ -0,0 +1,33 @@
+const pageTitle = 'spa سون لرن'
+const router = {
+  404: {
+    template: "./404.html",
+    title: ` پیدا نشد | ${pageTitle}`,
+  },
+  "/": {
+    template: "./index.html",
+    title: ` خانه | ${pageTitle}`,
+  },
+  "/blog": {
+    template: "./blog.html",
+    title: ` بلاگ | ${pageTitle}`,
+  },
+  "/courses": {
+    template: "./courses.html",
+    title: ` دورها | ${pageTitle}`,
+  },
+  "/product": {
+    template: "./product.html",
+    title: `  جزئیات دوره | ${pageTitle}`,
+  },
+  "/article": {
+    template: "./article.html",
+    title: ` مقاله | ${pageTitle}`,
+  },
+  "/cart": {
+    template: "./cart.html",
+    title: ` سبد خرید | ${pageTitle}`,
+  },
+};
+
+export default router;
